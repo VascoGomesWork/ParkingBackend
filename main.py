27ce63268184806487a53de5733608f9942b594b
Vasco Gomes <vasco.gomes2000@gmail.com>
2025-11-01 19:52:50 +0000

    Update

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 #from routes import router
 from routes.parks import router as parks_router
 
 app = FastAPI(title="Hackaton_Park")
 #app.include_router(router)
 app.include_router(parks_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # React app origin
+    allow_credentials=True,
+    allow_methods=["*"],  # allow all HTTP methods
+    allow_headers=["*"],  # allow all headers
+)
